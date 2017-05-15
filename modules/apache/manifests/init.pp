c0fefcca19cd7d303b22591cd848fd0ef463f5aa
eethak2 <eethak2@gmail.com>
2017-05-15 21:02:32 +0000

    kommitti

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,29 @@
+class apache {
+	
+	package { 'apache2':
+	ensure => 'installed',
+	allowcdrom => 'true',
+	}
+
+	package { 'vlc':
+	ensure => 'installed',
+	allowcdrom => 'true',
+	}
+
+	package { 'libreoffice':
+	ensure => 'installed',
+	allowcdrom => 'true',
+	}
+
+service {'apache2':
+	ensure => 'running',
+	require => Package ['apache2'],
+	}
+
+exec { 'userdir':
+	notify => Service['apache2'],
+	command => '/usr/sbin/a2enmod userdir',
+	require => Package['apache2'],
+	}
+
+}
